e81ae5e90580c3d9ab48ce69e2c9585bb2761559
luizasilveira <luizars7@hotmail.com>
2021-08-25 17:07:16 -0300

    main

---
+++ main.py
@@ -0,0 +1,110 @@
+from rply import LexerGenerator
+from rply.token import BaseBox
+from rply import ParserGenerator
+
+lg = LexerGenerator()
+#lg.add('NUMBER', r'[+-]?\d+')
+lg.add('NUMBER', r'\d+')
+lg.add('PLUS', r'\+')
+lg.add('MINUS', r'-')
+# lg.add('MUL', r'\*')
+# lg.add('DIV', r'/')
+# lg.add('POT', r'\^')
+lg.add('OPEN_PARENS', r'\(')
+lg.add('CLOSE_PARENS', r'\)')
+lg.ignore('\s+')
+lexer = lg.build()
+
+# for token in lexer.lex('3 + 2 * 3 ^ 1'):
+#   print(token)
+
+class Number(BaseBox):
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        return self.value
+
+class BinaryOp(BaseBox):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Add(BinaryOp):
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+class Sub(BinaryOp):
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+class Mul(BinaryOp):
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+class Div(BinaryOp):
+    def eval(self):
+        return self.left.eval() / self.right.eval()
+
+class Pot(BinaryOp):
+    def eval(self):
+        return self.left.eval() ** self.right.eval()
+
+pg = ParserGenerator(
+    # A list of all token names, accepted by the parser.
+    # ['NUMBER', 'OPEN_PARENS', 'CLOSE_PARENS',
+    #  'PLUS', 'MINUS', 'MUL', 'DIV', 'POT'
+    # ],
+    ['NUMBER', 'OPEN_PARENS', 'CLOSE_PARENS',
+     'PLUS', 'MINUS'
+    ],
+    # A list of precedence rules with ascending precedence, to
+    # disambiguate ambiguous production rules.
+    precedence=[
+        ('left', ['PLUS', 'MINUS'])
+        # ('left', ['MUL', 'DIV']),
+        # ('left', ['POT'])
+    ]
+)
+
+@pg.production('expression : NUMBER')
+def expression_number(p):
+    # p is a list of the pieces matched by the right hand side of the
+    # rule
+    return Number(float(p[0].getstr()))
+
+@pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
+def expression_parens(p):
+    return p[1]
+
+@pg.production('expression : expression PLUS expression')
+@pg.production('expression : expression MINUS expression')
+# @pg.production('expression : expression MUL expression')
+# @pg.production('expression : expression DIV expression')
+# @pg.production('expression : expression POT expression')
+def expression_binop(p):
+    left = p[0]
+    right = p[2]
+    if p[1].gettokentype() == 'PLUS':
+        return Add(left, right)
+    elif p[1].gettokentype() == 'MINUS':
+        return Sub(left, right)
+    # elif p[1].gettokentype() == 'MUL':
+    #     return Mul(left, right)
+    # elif p[1].gettokentype() == 'DIV':
+    #     return Div(left, right)
+    # elif p[1].gettokentype() == 'POT':
+    #     return Pot(left, right)
+    else:
+        raise AssertionError('Oops, this should not be possible!')
+
+parser = pg.build()
+
+
+def main(entrada):
+    result = parser.parse(lexer.lex(entrada)).eval()
+    print(int(result))
+
+
+if __name__ == "__main__":
+    main(input())
