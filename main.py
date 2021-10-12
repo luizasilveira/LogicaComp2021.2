b9d8793bf871b4156998237ba016a01dd26cf693
Daniel Roriz <brazdaniel@iMac-de-Daniel.local>
2021-10-12 20:30:18 -0300

    v2.1.1

--- main.py
+++ main.py
@@ -18,9 +18,16 @@ lg.add('DIV', r'/')
 lg.add('POT', r'\^')
 lg.add('OPEN_PARENS', r'\(')
 lg.add('CLOSE_PARENS', r'\)')
+lg.add('PRINT', r'imprimir')
+lg.add("IDENTIFIER", r'[a-zA-Z]*([a-zA-Z]|/d+|_)')
+lg.add('EQUAL', r'\=')
+lg.add('SEMICOLON', r'\;')
+
+
 lg.ignore('\s+')
 lg.ignore("/\*.*?\*/")
 lexer = lg.build()
+
 class Node(BaseBox):
     def __init__(self,value):
         self.value = value
@@ -66,6 +73,8 @@ class IntVal(Node):
     def eval(self):
         return int(self.value)
 
+
+
 # class NoOp(Node):
 #     def __init__(self, value):
 #         self.value = value
@@ -80,10 +89,17 @@ class Number(BaseBox):
     def eval(self):
         return self.value
 
+class Print():
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        print(self.value)
+
 pg = ParserGenerator(
     # A list of all token names, accepted by the parser.
     ['NUMBER', 'OPEN_PARENS', 'CLOSE_PARENS',
-     'PLUS', 'MINUS',"MUL","DIV", "POT"
+     'PLUS', 'MINUS',"MUL","DIV", "POT","IDENTIFIER","EQUAL","SEMICOLON"
     ],
     # A list of precedence rules with ascending precedence, to
     # disambiguate ambiguous production rules.
@@ -94,6 +110,23 @@ pg = ParserGenerator(
     ]
 )
 
+@pg.production('expression : NUMBER')
+def expression_number(p):
+    # p is a list of the pieces matched by the right hand side of the
+    # rule
+    return IntVal(p[0].getstr())
+
+# @pg.production('expression : IDENTIFIER EQUAL NUMBER SEMICOLON')
+# def expression_var(p):
+#     # p is a list of the pieces matched by the right hand side of the
+#     # rule
+#     ident = p[0].getstr()
+#     value = IntVal(p[2].getstr())
+#     dict = {}
+#     dict[ident] = value
+#     print("dicionario",dict)
+#     return value
+
 @pg.production('expression : PLUS expression')
 @pg.production('expression : MINUS expression')
 def expression_unary(p):
@@ -102,13 +135,6 @@ def expression_unary(p):
         return UnOp("PLUS", p[1])
     elif left.gettokentype() == 'MINUS':
         return UnOp("MINUS", p[1])
-
-@pg.production('expression : NUMBER')
-def expression_number(p):
-    # p is a list of the pieces matched by the right hand side of the
-    # rule
-    return IntVal(p[0].getstr())
-    #return Number(float(p[0].getstr()))
 
 @pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
 def expression_parens(p):
@@ -123,18 +149,34 @@ def expression_parens(p):
 def expression_binop(p):
     left = p[0]
     right = p[2]
-    if p[1].gettokentype() == 'PLUS':
-        return BinOp("PLUS",left, right)
-    elif p[1].gettokentype() == 'MINUS':
-        return BinOp('MINUS',left, right)
-    elif p[1].gettokentype() == 'MUL':
-        return BinOp('MUL',left, right)
-    elif p[1].gettokentype() == 'DIV':
-        return BinOp('DIV',left, right)
-    elif p[1].gettokentype() == 'POT':
-        return BinOp('POT',left, right)
-    else:
-        raise AssertionError('Oops, this should not be possible!')
+    return BinOp(p[1].gettokentype(),left,right)
+
+# expr ::= (+ NUMBER NUMBER)
+
+# expr ::= (+ NUMBER expr) | (+ expr NUMBER) | (+ expr expr)
+
+# @pg.production('expr : OPEN_PARENS PLUS NUMBER NUMBER CLOSE_PARENS')
+# def expr_num(p):
+#     num1 = Number(int(p[2].getstr()))
+#     num2 = Number(int(p[3].getstr()))
+#     return Add(num1,num2)  
+
+# @pg.production('expr : OPEN_PARENS PLUS NUMBER expr CLOSE_PARENS')
+# def expr_numExpr(p):
+#     number = Number(int(p[2].getstr()))  
+#     return Add(number,p[3])  
+
+# expressão = termo + termo |termo - termo | termo
+# termo = fator * fator | fator / fator | fator
+# fator = NUMBER | expressão
+
+#var = IDENTIFIER EQUAL NUMBER SEMICOLON
+
+
+
+    # print("valor",IntVal(p[0].getstr()))
+    # return IntVal(p[0].getstr())
+
 
 parser = pg.build()
 
@@ -143,5 +185,5 @@ def main(entrada):
     print(int(result))
 
 if __name__ == "__main__":
-    main(sys.argv[1])
-    #main(input())
+    #main(sys.argv[1])
+    main(input())
